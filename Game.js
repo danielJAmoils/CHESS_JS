d50013fffecb3a5f009024516782f1fdb866f9c9
Daniel Amoils <danieljamoils@gmail.com>
2020-05-01 19:27:53 -0400

    Changed to using table for board because I had a headache from using divs

--- Game.js
+++ Game.js
@@ -4,36 +4,20 @@ class Game{
     }
     createBoard(){
         //function for creating chess board layout
-        let count = 0,
-        row = 1,
-        startColor = 0
-        //startColor:0 is black,1 is white
-        while (count < 64){
-            if (count % 8 == 0){
-                row++
-                if(row%2==0){
-                    //white start row
-                    document.write('<br/><div style="background-color:##FFFFFF;float:left;">&nbsp</div>')
-                    for(let i = 0;i<=7;i++){
-                        if(i%2==1){
-                            document.write('<div style="background-color:##FFFFFF;float:left;">&nbsp</div>') 
-                        }else{
-                            document.write('<div style="background-color:#000000;float:left;">&nbsp</div>')
-                        }
-                    }
-                }else{
-                     //black start row
-                    document.write('<br/><div style="background-color:#000000;float:left;">&nbsp</div>')
-                    for(let i = 0;i<=7;i++){
-                        if(i%2==1){
-                            document.write('<div style="background-color:#000000;float:left;">&nbsp</div>') 
-                        }else{
-                            document.write('<div style="background-color:##FFFFFF;float:left;">&nbsp</div>')
-                        }
-                    }
+        var table = document.createElement("table");
+        for (var i = 1; i < 9; i++) {
+            var tr = document.createElement('tr');
+            for (var j = 1; j < 9; j++) {
+                var td = document.createElement('td');
+                if (i%2 == j%2) {
+                    td.className = "white";
+                } else {
+                    td.className = "black";
                 }
+                tr.appendChild(td);
             }
-            count++
+            table.appendChild(tr);
         }
+        document.body.appendChild(table);
     }
 }
