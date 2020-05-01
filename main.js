b12f9af594cf8e3cb07ef21cd71a27591c6be3e6
Daniel Amoils <danieljamoils@gmail.com>
2020-05-01 16:49:19 -0400

    Begin setting up project

---
+++ main.js
@@ -0,0 +1,4 @@
+//new board line document.write('<br/><div style="background-color:#000000;float:left;">&nbsp</div>');
+//black square document.write('<div style="background-color:#000000;float:left;">&nbsp</div>');    
+//white square document.write('<div style="background-color:#FFFFFF;float:left;">&nbsp</div>');
+
