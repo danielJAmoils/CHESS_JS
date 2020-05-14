e19f13bd6b8caf163caec3f49bf18f60e44face0
Daniel Amoils <danieljamoils@gmail.com>
2020-05-14 15:06:20 -0400

    added queen movement checks

--- Pieces/Queen.js
+++ Pieces/Queen.js
@@ -3,7 +3,15 @@ class Queen extends Piece{
         super(color,x,y,"♕","♛")
         super.drawPiece()
     }
-    correctMovement(){
-        return true
+    correctMovement(newX,newY){
+        newX = parseInt(newX)
+        newY = parseInt(newY)//changes to numbers
+        if(Math.abs(this.x-newX) === Math.abs(this.y-newY)){//bishop like movement
+            return true
+        }else if((this.x === newX && this.y !== newY)||(this.x !== newX && this.y === newY)){//rook like movement
+            return true
+        }else{
+            return false
+        }
     }
 }
